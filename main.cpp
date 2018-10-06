6b115e969088f45c3189a21a54d2d0f8315c2b06
jankodedic7 <jankodedic7@gmail.com>
2018-10-06 10:38:54 +0200

    Problem: No debug output for the card class.
    Solution: Overload the ostream operator<< for the card class.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 
+#include <poker/card.hpp>
+
+namespace poker {
+
+std::ostream&
+operator<<(std::ostream& os, card c)
+{
+    constexpr char rank_symbols[] = {
+        '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'
+    };
+    constexpr char suit_symbols[] = { 5, 4, 3, 6 };
+    const auto rank_symbol = rank_symbols[static_cast<std::size_t>(c.rank)];
+    const auto suit_symbol = suit_symbols[static_cast<std::size_t>(c.suit)];
+    return os << rank_symbol << suit_symbol;
+}
+
+} // namespace poker
+
 int
 main()
 {
-    std::cout << "Hello Janko!\n";
+    using namespace poker;
+    const auto c1 = card{card_rank::A, card_suit::clubs};
+    const auto c2 = card{card_rank::A, card_suit::diamonds};
+    const auto c3 = card{card_rank::A, card_suit::hearts};
+    const auto c4 = card{card_rank::A, card_suit::spades};
+    std::cout << c1 << std::endl;
+    std::cout << c2 << std::endl;
+    std::cout << c3 << std::endl;
+    std::cout << c4 << std::endl;
 }
