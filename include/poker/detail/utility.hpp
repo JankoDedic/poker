3fec9e0e1a4615a927017a765d82a44af3f6ee20
Janko Dedic <jankodedic7@gmail.com>
2019-03-24 11:17:02 +0100

    Problem: Missing some flag operations
    Solution: Add ^, ^= and ~ operations to the macro

--- include/poker/detail/utility.hpp
+++ include/poker/detail/utility.hpp
@@ -3,6 +3,11 @@
 #include <type_traits>
 
 #define POKER_DETAIL_DEFINE_FRIEND_FLAG_OPERATIONS(Type)                      \
+    friend constexpr auto operator~(Type x) noexcept -> Type {                \
+        using poker::detail::to_underlying;                                   \
+        return static_cast<Type>(~to_underlying(x));                          \
+    }                                                                         \
+                                                                              \
     friend constexpr auto operator|(Type x, Type y) noexcept -> Type {        \
         using poker::detail::to_underlying;                                   \
         return static_cast<Type>(to_underlying(x) | to_underlying(y));        \
@@ -13,11 +18,20 @@
         return static_cast<Type>(to_underlying(x) & to_underlying(y));        \
     }                                                                         \
                                                                               \
+    friend constexpr auto operator^(Type x, Type y) noexcept -> Type {        \
+        using poker::detail::to_underlying;                                   \
+        return static_cast<Type>(to_underlying(x) ^ to_underlying(y));        \
+    }                                                                         \
+                                                                              \
     friend constexpr auto operator|=(Type &x, Type y) noexcept -> Type & {    \
         return x = x | y;                                                     \
     }                                                                         \
                                                                               \
     friend constexpr auto operator&=(Type &x, Type y) noexcept -> Type & {    \
+        return x = x & y;                                                     \
+    }                                                                         \
+                                                                              \
+    friend constexpr auto operator^=(Type &x, Type y) noexcept -> Type & {    \
         return x = x & y;                                                     \
     }
 
