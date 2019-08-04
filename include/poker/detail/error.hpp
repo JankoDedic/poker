db8f337b610e72462e6f8cd0ec89d40fe19b97b8
Janko Dedic <jankodedic7@gmail.com>
2019-08-04 13:23:18 +0200

    Problem: std::invalid_argument is more specific than std::logic_error
    Solution: Replace the exception type.

--- include/poker/detail/error.hpp
+++ include/poker/detail/error.hpp
@@ -6,7 +6,7 @@
 #ifdef POKER_THROW_ON_ERROR
 #   define POKER_DETAIL_THROWS_ON_ERROR 1
 #   define POKER_NOEXCEPT
-#   define POKER_DETAIL_ASSERT(condition, explanation) if (!(condition)) throw std::logic_error{explanation}
+#   define POKER_DETAIL_ASSERT(condition, explanation) if (!(condition)) throw std::invalid_argument{explanation}
 #else
 #   define POKER_NOEXCEPT noexcept
 #   define POKER_DETAIL_ASSERT(condition, explanation) assert((condition) && explanation)
