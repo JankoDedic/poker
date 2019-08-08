40a3765be3a7efd0059094739a25af7ecf57152e
Janko Dedic <jankodedic7@gmail.com>
2019-08-08 14:46:54 +0200

    Problem: pot misbehaves when no bets are placed.
    It appends all the players to _eligible_players, including the ones who
    are already in.
    
    Solution: clear the _eligible_players before re-adding all the players.
    As explained in the comments, it is possible that a player folds on a
    no-bet betting round. This is why instead of no-op, we re-add all the
    indices over again.
    Some additional comments have also been added to aid in understanding
    this function.
    Regression test is included.

--- include/poker/pot.hpp
+++ include/poker/pot.hpp
@@ -28,19 +28,26 @@ public:
     }
 
     auto collect_bets_from(seat_array_view players) noexcept -> chips {
+        // Find the first player who has placed a bet.
         auto it = std::find_if(players.begin(), players.end(), [] (const auto& p) { return p.bet_size() != 0; });
         if (it == players.end()) {
+            // If no players have bet, just make all the players who are still in the pot eligible.
+            // It is possible that some player has folded even if nobody has bet.
+            // We would not want to keep him as an eligible player.
+            _eligible_players.clear();
             for (auto iter = players.begin(); iter != players.end(); ++iter) {
                 _eligible_players.push_back(iter.index());
             }
             return 0;
         } else {
+            // Find the smallest player bet on the table.
             auto min_bet = (*it).bet_size();
             std::for_each(it, players.end(), [&] (const auto& p) {
                 if (p.bet_size() != 0 && p.bet_size() < min_bet) {
                     min_bet = p.bet_size();
                 }
             });
+            // Deduct that bet from all the players, and add it to the pot.
             _eligible_players.clear();
             for (auto iter = players.begin(); iter != players.end(); ++iter) {
                 if ((*iter).bet_size() != 0) {
--- tests/poker/detail/pot.test.cpp
+++ tests/poker/detail/pot.test.cpp
@@ -33,3 +33,16 @@ TEST_CASE("no bets remaining") {
     REQUIRE_EQ(p.size(), 0);
     REQUIRE_EQ(p.eligible_players().size(), 3);
 }
+
+TEST_CASE("Players who folded are not kept as eligible after a betting round with no bets") {
+    auto players = seat_array{};
+    players.add_player(0, player{100});
+    players.add_player(1, player{100});
+    players[0].bet(10);
+    players[1].bet(10);
+    auto p = pot{};
+    p.collect_bets_from(players);
+    players.remove_player(1);
+    p.collect_bets_from(players);
+    REQUIRE_EQ(p.eligible_players().size(), 1);
+}
